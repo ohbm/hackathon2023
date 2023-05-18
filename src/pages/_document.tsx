b1edae142ce3b45312562d2cbe13a00984a2863c
Anibal <anibalsolon@gmail.com>
2023-05-18 16:46:41 -0500

    fix: dont need this

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document({ __NEXT_DATA__ }) {
+export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={__NEXT_DATA__?.props?.bodyClassName ?? ''}>
+      <body>
         <Main />
         <NextScript />
       </body>
