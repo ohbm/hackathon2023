0e2545a001d1e2b53dee6b571f0825ac41e0cf9d
Anibal Sólon <anibalsolon@gmail.com>
2023-07-19 11:09:11 -0500

    enh: new dsc link

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
     REGISTRATION_URL: 'https://www.humanbrainmapping.org/i4a/ams/meetings/index.cfm?controller=meetings&action=startRegistration&conferenceID=146&reginit=1&pageID=4217',
     PROJECTS_SUBMISSION_URL: 'https://github.com/ohbm/hackathon2023/issues/new?labels=Hackathon+Project&projects=&template=hackathon-project-form.yml&title=%3CMy+Project+Name%3E',
 
-    DISCORD_URL: 'https://discord.gg/qUzW56dZT2',
+    DISCORD_URL: 'https://discord.gg/ohbmbrainhack2023',
     TWITTER_URL: 'https://twitter.com/ohbmopen',
     MATTERMOST_URL: 'https://mattermost.brainhack.org/brainhack/channels/hbm-hackathon',
     GITHUB_URL: 'https://github.com/ohbm/',
