cceb0f812cd3a1f45f7e24cf9ec776ed898dd10c
Anibal Solon <anibalsolon@gmail.com>
2024-12-18 15:53:51 +0300

    enh: add images and refs

--- manuscript/compile.sh
+++ manuscript/compile.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+if [[ " $* " == *" --clean "* ]]; then
+    git clean -Xfd .
+fi
+
 docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "blang/latex:ubuntu" pdflatex -interaction=nonstopmode -output-directory=. main.tex
+docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "blang/latex:ubuntu" bibtex main
+docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "blang/latex:ubuntu" pdflatex -interaction=nonstopmode -output-directory=. main.tex
+docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "blang/latex:ubuntu" pdflatex -interaction=nonstopmode -output-directory=. main.tex
+
+docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "blang/latex:ubuntu" pdflatex -interaction=nonstopmode -output-directory=. supplementary.tex
